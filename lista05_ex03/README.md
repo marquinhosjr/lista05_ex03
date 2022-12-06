a720a19e895fe65a525f5f480b35783b08f2ef12
Marcos Masquetti <marquinhosmasquettij@hotmail.com>
2022-12-06 19:32:32 -0300

    exercicio 3

---
+++ lista05_ex03/README.md
@@ -0,0 +1,5 @@
+## Projeto atividade Java - GitHub
+## Enunciado
+Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+## Dependency Management
+The 'JAVA PROJECTS' view allows you to manage your dependencies. More details can be found [here].
---
+++ lista05_ex03/src/App.java
@@ -0,0 +1,13 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(tresArgumentos(200, 200, 200));
+    }
+
+    public static int tresArgumentos(int a, int b, int c) {
+        int soma = a + b + c;
+        return soma;
+    }
+}  
+    
